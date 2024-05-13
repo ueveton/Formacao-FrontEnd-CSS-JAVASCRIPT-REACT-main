e0ac7e4bfd84205c93b5c104b4e1171bf17ca747
ueveton <uevetonsp89@gmail.com>
2024-05-13 12:34:59 -0300

    Operadores Atitimeticos

---
+++ JS/3_Operadores/js/scripts.js
@@ -0,0 +1,42 @@
+// 1 - Number
+console.log("1 - Number")
+console.log(typeof 2)
+console.log(typeof 5.14)
+console.log(typeof -3)
+
+// 2 - Operações aritimeicas
+console.log("2 - Operações aritimeicas")
+console.log("Calculo de soma 2 + 4 = " + 2 + 4)
+console.log("Calculo de subtração 10 - 5 = " + 10-5)
+console.log("Calculo de multiplicação 10 * 5 = " + 10*5)
+console.log("Calculo de divisão 10 / 5 = " + 10/2)
+console.log("calcule (2+(10*15)) = " + (2+(10*15)));
+
+// 3 - Numeros especias ou Speial Numbers
+console.log("3 - Numeros especias")
+console.log("Tipo de dados Infinity = " + typeof Infinity)
+console.log("Tipo de dados -Infinity  = " + typeof -Infinity)
+console.log("Tipo de dados NaN não calcula numeros com letras = " + 12 -  "asd")
+console.log("Tipo de dados NaN = " + typeof NaN)
+
+// 4 - String
+console.log("4 - String")
+console.log ("Um texto com aspas simples")
+console.log (`Um texto com crase`)
+console.log (`13`)
+console.log(typeof "Um texto com aspas simples")
+console.log (typeof `Um texto com crase`)
+console.log (typeof `13`)
+
+// 5 - Simbolos especiais em string
+console.log("5 - Simbolos especiais em string")
+console.log ("testeando a  \n quebra de linha")
+console.log ("Espaçamento \t tab")
+
+// 6 - Concatenação
+console.log("6 - Concatenação")
+console.log("oi, " + "tudo " + "bem")
+console.log(`Testando ` + `com ` + `crase`)
+
+// 7 - Template Strings
+console.log("7 - Template Strings")
