f20eecdd4f261ec6bc8a2e6e35fe2ad7f1ce537a
ueveton <uevetonsp89@gmail.com>
2024-05-24 17:07:56 -0300

    Estrutura de programação

---
+++ JS/4_Estrutura/js/scripts.js
@@ -0,0 +1,44 @@
+// 1 -variaveis
+let nome = "Ueveton";
+
+console.log(nome);
+
+nome = "Ueveton Soares Pereira";
+console.log(nome);
+
+// const = não muda de valor
+const idade = 34;
+console.log(idade);
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+// 2 - mais sobre variaveis
+let a = 10, b = 20, c = 30;
+console.log(a, b, c);
+
+const nomecompleto = "Ueveton Soares";
+const nomeCompleto = "Heitor da Silva";
+
+console.log(nomecompleto +`, `+ nomeCompleto);
+
+let _teste = "ok";
+let $teste = "ok";
+
+console.log(_teste +`, `+ $teste);
+
+// 3 - prompt
+/*const age = prompt("Digite a sua idade: ");
+console.log(`Você tem ${age} anos.`);*/
+// travado pois iria atrapalhar o alert
+
+// 4 - alert
+/*alert("Testando o alert")
+const z = 10;
+alert(`O numero é ${z}`);*/
+// travado para dar procedimento ao curso
+
+// 5 - math
+console.log(Math.max (5,4,3,8,7));
+console.log(Math.floor(5.14));
+console.log(Math.ceil(5.14));
