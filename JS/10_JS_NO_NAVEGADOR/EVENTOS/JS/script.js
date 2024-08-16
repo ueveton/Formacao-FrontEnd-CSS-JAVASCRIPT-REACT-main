65cd347abb99c081d9e7a1a0847e77f242463ed9
ueveton <uevetonsp89@gmail.com>
2024-08-16 08:24:22 -0300

    Final seção eventos. Abortdado preventdeful, eventos de mous e teclado

--- JS/10_JS_NO_NAVEGADOR/EVENTOS/JS/script.js
+++ JS/10_JS_NO_NAVEGADOR/EVENTOS/JS/script.js
@@ -35,3 +35,111 @@ myTitle.addEventListener("click", (e)=>{
 
 // 4 - propagação
 console.log("---- 4 - propagação ----");
+
+const containerBtn = document.querySelector("#btn-container");
+const btnInsideContainer = document.querySelector("#div-btn")
+
+containerBtn.addEventListener("click", ()=>{
+    console.log("Evento 1")
+})
+
+btnInsideContainer.addEventListener("click", (e)=>{
+    e.stopPropagation();
+    console.log("Evento 2");
+})
+
+// 5 - preventDefault
+console.log("---- 5 - preventDefault ----");
+
+const a = document.querySelector("a")
+a.addEventListener("click", (e)=>{
+    e.preventDefault();
+    console.log("Não vou para a pagina")
+})
+
+// 6 - Ações do teclado
+console.log("---- 6 - Ações do teclado ----");
+
+document.addEventListener("keyup", (e) => {
+    console.log(`Soltou a tecla ${e.key}`);
+})
+document.addEventListener("keydown", (e) => {
+    console.log(`Apertou a tecla ${e.key}`);
+})
+
+// 7 - Ações do mouse
+console.log("---- 7 - Ações do mouse ----")
+
+const mouse = document.querySelector("#mouse")
+mouse.addEventListener("mousedown", (e) =>{
+    console.log("Precionou o botão!");
+})
+mouse.addEventListener("mouseup", (e) =>{
+    console.log("Soltou o botão!");
+})
+mouse.addEventListener("dblclick", (e) =>{
+    console.log("Duplo click no botão!");
+})
+
+// 8 - Movimento do mouse
+console.log("---- 8 - Movimento do mouse ----")
+
+document.addEventListener("mousemove", (e) =>{
+    // console.log(`No eico x ${e.x}`);
+    // console.log(`No eico x ${e.y}`);
+})
+
+// 9 - Eventos por scroll
+console.log("---- 9 - Eventos por scroll ----")
+
+window.addEventListener("scroll", (e) => {
+    if (window.pageYOffset > 20){
+        console.log("Passamos do 20px")
+    }
+})
+
+// 10 - Eventos por foco
+console.log("---- 10 - Eventos por foco ----")
+
+const input = document.querySelector("#my-input")
+input.addEventListener("focus", (e) =>{
+    console.log("Entrou no input")
+})
+input.addEventListener("blur", (e) =>{
+    if (input.value = isNaN){
+        alert("Preencher o campo")
+    }
+    console.log("Saiu do input")
+})
+
+// 11 - Eventos por carregamento de pagina
+console.log("---- 11 - Eventos por carregamento de pagina ----")
+
+window.addEventListener("load", ()=>{
+    console.log("A pagina carregou")
+})
+
+window.addEventListener("beforeload", (e)=>{
+    e.preventDefault();
+    e.returnValue = "";
+})
+
+// 12 - tecnica debounce
+console.log("---- 12 - tecnica debounce ----")
+
+const debounce = (f, delay) =>{
+    let timeout
+    return(...arguments) =>{
+        if(timeout){
+            clearTimeout(timeout)
+        }
+
+        timeout = setTimeout(()=>{
+            f.apply(arguments)
+        }, delay)
+    }
+}
+
+window.addEventListener("mousemove", debounce (()=>{
+    console.log("Executando evento 400ms")
+}, 400));
