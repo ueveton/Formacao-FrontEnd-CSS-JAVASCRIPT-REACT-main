f4b292a9ef8589d39ffae668b7da23533c398691
ueveton <uevetonsp89@gmail.com>
2024-08-21 09:21:38 -0300

    Conclusão do projeto tabuada JS

--- JS/Projeto Tabuada/js/script.js
+++ JS/Projeto Tabuada/js/script.js
@@ -2,8 +2,30 @@
 const multiplicationForm = document.querySelector("#multiplicacao-form")
 const numberInput = document.querySelector("#number")
 const multiplicationInput = document.querySelector("#multiplicator")
+const operation = document.querySelector("#multiplication-operations")
+const multiplicationTable = document.querySelector("#multiplication-table")
+const multiplicationTitle = document.querySelector("#multiplication-title span")
 
 //FUNCOES
+const creatTable = (number, multiplicatornInput) =>{
+    operation.innerHTML = "";
+    for(i = 1; i <= multiplicatornInput; i++) {
+        const result = number * i;
+        const calcular = `
+            <div class="row">
+                <div class="operation">
+                    ${number} x ${i} = 
+                </div>
+                <div class="result">${result}</div>
+            </div>
+        `;
+        const parse = new DOMParser();
+        const htmlcalcular = parse.parseFromString(calcular, "text/html");
+        const row = htmlcalcular.querySelector(".row");
+        operation.appendChild(row);
+    }
+    multiplicationTitle.innerText = number;
+}
 
 //EVEMTOS
 multiplicationForm.addEventListener("click", (e) => {
@@ -11,5 +33,5 @@ multiplicationForm.addEventListener("click", (e) => {
     const multiplicationNunber = +numberInput.value;
     const multiplicatornInput = +multiplicationInput.value;
     if(!multiplicationNunber || !multiplicatornInput)return
-    console.log(multiplicationNunber, multiplicatornInput)
+    creatTable(multiplicationNunber, multiplicatornInput)
 })
