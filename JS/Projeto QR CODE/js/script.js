f28379846234ec7ea666693a771b3a7367c49ea5
ueveton <uevetonsp89@gmail.com>
2024-08-22 17:31:19 -0300

    iniciando js

---
+++ JS/Projeto QR CODE/js/script.js
@@ -0,0 +1,37 @@
+const classContainer = document.querySelector(".container");
+const formInput = document.querySelector("#qr-form input");
+const formBtn = document.querySelector("#qr-form button");
+const qrImg = document.querySelector("#qr-code img");
+
+function generatorQrCode (){
+    const formInputValue = formInput.value;
+    if(!formInputValue)return
+    formBtn.innerText ="Gerando codigo..."
+    qrImg.src = `
+        https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${formInputValue}
+    `;
+    loadQrCode();
+};
+
+function loadQrCode (){
+    classContainer.classList.add("active")
+    formBtn.innerText ="Codigo gerado!";
+};
+
+function remove
+
+formBtn.addEventListener("click", () => {
+    generatorQrCode()
+});
+
+formInput.addEventListener("keydown", (e) => {
+    if(e.code === "Enter"){
+        generatorQrCode()
+    }
+});
+
+formInput.addEventListener("keyup", () => {
+    if(!formInput.value){
+        
+    }
+})
