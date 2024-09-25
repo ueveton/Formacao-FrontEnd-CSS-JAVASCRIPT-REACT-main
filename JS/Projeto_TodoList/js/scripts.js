7279fd0dc094f33bd2e47ebb625421977e7a3f9f
Ueveton <uevetonsp@gmail.com>
2024-09-25 20:13:53 -0300

    criando js todolist

---
+++ JS/Projeto_TodoList/js/scripts.js
@@ -0,0 +1,28 @@
+const todoForm = document.querySelector('#todo-form');
+const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todo-list');
+const editForm = document.querySelector('#edit-form');
+const editInput = document.querySelector('#edit-input');
+const cancelEditBtn = document.querySelector('#cancel-edit-btn');
+
+todoForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const inputValue = todoInput.value;
+    if (inputValue){
+        saveTodo(inputValue)
+    }
+})
+
+const saveTodo = (text) =>{
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle)
+
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = 'fa-solid fa-check';
+    todo.appendChild(doneBtn)
+}
