5c2e91ca7ed2f82bbb6afedca299728aa5b0fe96
ueveton <uevetonsp89@gmail.com>
2024-09-26 12:43:50 -0300

    manipulando js

--- JS/Projeto_TodoList/js/scripts.js
+++ JS/Projeto_TodoList/js/scripts.js
@@ -1,11 +1,13 @@
-const todoForm = document.querySelector('#todo-form');
-const todoInput = document.querySelector('#todo-input');
-const todoList = document.querySelector('#todo-list');
-const editForm = document.querySelector('#edit-form');
-const editInput = document.querySelector('#edit-input');
-const cancelEditBtn = document.querySelector('#cancel-edit-btn');
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
-todoForm.addEventListener('submit', (e)=>{
+let oldInputValue;
+
+todoForm.addEventListener("submit", (e)=>{
     e.preventDefault();
     const inputValue = todoInput.value;
     if (inputValue){
@@ -23,6 +25,56 @@ const saveTodo = (text) =>{
 
     const doneBtn = document.createElement("button")
     doneBtn.classList.add("finish-todo");
-    doneBtn.innerHTML = 'fa-solid fa-check';
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
     todo.appendChild(doneBtn)
+
+    const editBtn = document.createElement("button")
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    todo.appendChild(editBtn)
+
+    const removeBtn = document.createElement("button")
+    removeBtn.classList.add("remove-todo");
+    removeBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    todo.appendChild(removeBtn)
+
+    todoList.appendChild(todo);
+    todoInput.value = "";
+    todoInput.focus();
 }
+
+document.addEventListener("click", (e)=>{
+    const targetEl = e.target;
+    const parentEl = targetEl.closest("div");
+    let todoTitle;
+
+    if (parentEl && parentEl.querySelector("h3")){
+        todoTitle = parentEl.querySelector("h3").innerText;
+    }
+
+    if (targetEl.classList.contains("finish-todo")){
+        parentEl.classList.toggle("done")
+    }
+
+    if (targetEl.classList.contains("edit-todo")){
+        toggleForms()
+
+        editInput.value = todoTitle
+        oldInputValue.value = todoTitle
+    }
+
+    if (targetEl.classList.contains("remove-todo")){
+        parentEl.remove()
+    }
+})
+
+const toggleForms = () =>{
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide");
+    todoList.classList.toggle("hide");
+}
+
+cancelEditBtn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    toggleForms();
+})
