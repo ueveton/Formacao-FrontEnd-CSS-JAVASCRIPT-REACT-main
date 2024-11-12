4a32b81deea70d05dbe0f1224316a89072f46238
ueveton <uevetonsp89@gmail.com>
2024-11-12 08:00:19 -0300

    getProducts setando os produtos do banco e visualização em tela.

--- REACT/7_HTTP_REACT/htto_react/src/App.jsx
+++ REACT/7_HTTP_REACT/htto_react/src/App.jsx
@@ -1,12 +1,50 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+const url = "http://localhost:3000/products"
+
 function App() {
-  const [count, setCount] = useState(0)
+  // 1 - Resgatando dados
+  const [products, setProducts] = useState([])
+
+  useEffect(()=> {
+    async function getData(){
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data)
+    }
+    getData()
+  },[])
+
+  // 2 - Envio de dados
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
 
   return (
     <div className='App'>
       <h1>Http em React</h1>
+      {/* {1 - resgate de dados } */}
+      <ul>
+        {products.map((products) => (
+          <li key={products.id}>
+              {products.name} - R$ {products.price}
+          </li>
+        ))}
+      </ul>
+      {/* 2 - Enviando dados */}
+      <div className="add-product">
+        <form>
+          <label>
+            <span>Nome:</span>
+            <input type="text" value={name} onChange={(e)=> setName(e.target.value)} />
+          </label>
+          <label>
+            <span>Preço:</span>
+            <input type="text" value={price} onChange={(e)=> setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="Enviar" />
+        </form>
+      </div>
     </div>
   )
 }
